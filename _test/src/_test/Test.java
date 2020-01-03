d55fcd10ff65dab8b5177993818dce39f6b99f80
黎振宇 <710273561@qq.com>
2020-01-03 12:05:54 +0800

    1234

--- _test/src/_test/Test.java
+++ _test/src/_test/Test.java
@@ -4,6 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		String s= "aaa";
 		int a = 1;
+		int a1 = 1;
+		System.out.println(a1);
+		System.out.println(s);
+		System.out.println(a+s);
+		System.out.println(a);
 	}
 
 }
